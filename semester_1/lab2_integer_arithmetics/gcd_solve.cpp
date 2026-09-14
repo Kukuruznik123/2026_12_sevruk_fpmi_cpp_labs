8919102396b8d96855cda9a29249d1f9c8539805
Kukuruznik123 <arsenij09bizon@gmail.com>
2026-09-14 20:33:47 +0300

    Add files via upload

---
+++ semester_1/lab2_integer_arithmetics/gcd_solve.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <numeric>
+
+int Gcd(int a, int b) {
+    if (a && b) {
+        int rem = a % b;
+        a = b;
+        b = rem;
+        return Gcd(a, b);
+    }
+    return std::max(a, b);
+}
+
+void TryRead(int& number) {
+    if (!(std::cin >> number)) {
+        std::cout << "Fail on reading the number" << std::endl;
+        std::exit(0);
+    }
+}
+
+int main() {
+    int a, b;
+
+    std::cout << "Enter a: ";
+    TryRead(a);
+
+    std::cout << "Enter b: ";
+    TryRead(b);
+
+    if (a <= 0 || b <= 0) {
+        std::cout << "Numbers must be positive" << std::endl;
+        return 0;
+    }
+
+    int gcd1 = Gcd(std::max(a, b), std::min(a, b));
+    int gcd2 = std::gcd(a, b);
+    if (gcd1 != gcd2) {
+        std::cout << "You broke my program :(" << std::endl;
+        return 1;
+    } else {
+        std::cout << gcd1 << std::endl;
+    }
+
+    return 0;
+}
